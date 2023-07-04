53eed0f1a634e9573c6b72deee4d60dd50d35618
jaypee15 <okoyejohnpaul15@gmail.com>
2023-07-04 23:54:04 +0100

    modified login and register pages, used chakra ui

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,15 +3,36 @@ import { login } from '../utils/auth';
 import { useNavigate } from 'react-router-dom';
 import { useAuthStore } from '../store/auth';
 
-const Login = () => {
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Checkbox,
+    Stack,
+    Link,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
+    InputGroup,
+    InputRightElement,
+  } from '@chakra-ui/react';
+  import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+  
+
+  export default function Login() {
+
     const navigate = useNavigate();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+    const [showPassword, setShowPassword] = useState(false);
 
     useEffect(() => {
         if (isLoggedIn()) {
-            navigate('/');
+            navigate('/feed');
         }
     }, []);
 
@@ -26,38 +47,78 @@ const Login = () => {
         if (error) {
             alert(error);
         } else {
-            navigate('/');
+            navigate('/feed');
             resetForm();
         }
     };
-    return (
-        <section>
-            <h1>Login</h1>
-            <form onSubmit={handleLogin}>
-                <div>
-                    <label htmlFor="username">Username</label>
-                    <input
-                        type="text"
-                        id="username"
-                        name="username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <button type="submit">Login</button>
-            </form>
-        </section>
-    );
-};
 
-export default Login;
+
+    return (
+      <Flex
+        minH={'100vh'}
+        align={'center'}
+        justify={'center'}
+        bg={useColorModeValue('gray.50', 'gray.800')}>
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+          <Stack align={'center'}>
+            <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+            <Text fontSize={'lg'} color={'gray.600'}>
+              to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️
+            </Text>
+          </Stack>
+          <Box
+            rounded={'lg'}
+            bg={useColorModeValue('white', 'gray.700')}
+            boxShadow={'lg'}
+            p={8}>
+            <Stack spacing={4}>
+              <FormControl id="name">
+                <FormLabel>User name</FormLabel>
+                <Input type="name"
+                 onChange={(e) => setUsername(e.target.value)} />
+              </FormControl>
+              <FormControl id="password">
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                <Input type={showPassword ? 'text' : 'password'} 
+                onChange={(e) => setPassword(e.target.value)} />
+                <InputRightElement h={'full'}>
+                  <Button
+                    variant={'ghost'}
+                    onClick={() =>
+                      setShowPassword((showPassword) => !showPassword)
+                    }>
+                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+       
+              </FormControl>
+              <Stack spacing={10}>
+                <Stack
+                  direction={{ base: 'column', sm: 'row' }}
+                  align={'start'}
+                  justify={'space-between'}>
+               
+                </Stack>
+                <Button
+                onClick={handleLogin}
+                  bg={'blue.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'blue.500',
+                  }}>
+                  Sign in
+                </Button>
+              </Stack>
+            </Stack>
+            <Stack pt={6}>
+                <Text align={'center'}>
+                 Don't have an account? <Link href="/register" color={'blue.400'}>SignUp</Link>
+                </Text>
+              </Stack>
+          </Box>
+        </Stack>
+      </Flex>
+    );
+  }
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -3,7 +3,75 @@ import { register } from '../utils/auth';
 import { useNavigate } from 'react-router-dom';
 import { useAuthStore } from '../store/auth';
 
-function Register() {
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    InputGroup,
+    HStack,
+    InputRightElement,
+    Stack,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
+    Link,
+  } from '@chakra-ui/react';
+  import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+
+    // return (
+    //     <section>
+    //         <form onSubmit={handleSubmit}>
+    //             <h1>Register</h1>
+    //             <hr />
+    //             <div>
+    //                 <label htmlFor="username">Username</label>
+    //                 <input
+    //                     type="text"
+    //                     id="username"
+    //                     onChange={(e) => setUsername(e.target.value)}
+    //                     placeholder="Username"
+    //                     required
+    //                 />
+    //             </div>
+    //             <div>
+    //                 <label htmlFor="password">Password</label>
+    //                 <input
+    //                     type="password"
+    //                     id="password"
+    //                     onChange={(e) => setPassword(e.target.value)}
+    //                     placeholder="Password"
+    //                     required
+    //                 />
+    //             </div>
+    //             <div>
+    //                 <label htmlFor="confirm-password">Confirm Password</label>
+    //                 <input
+    //                     type="password"
+    //                     id="confirm-password"
+    //                     onChange={(e) => setPassword2(e.target.value)}
+    //                     placeholder="Confirm Password"
+    //                     required
+    //                 />
+    //                 <p>
+    //                     {password2 !== password ? 'Passwords do not match' : ''}
+    //                 </p>
+    //             </div>
+    //             <button type="submit">Register</button>
+    //         </form>
+    //     </section>
+    // );
+
+
+
+
+
+  
+  export default function Register() {
+    const [showPassword, setShowPassword] = useState(false);
+
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [password2, setPassword2] = useState('');
@@ -32,50 +100,69 @@ function Register() {
             navigate('/');
             resetForm();
         }
-    };
-
+    }
     return (
-        <section>
-            <form onSubmit={handleSubmit}>
-                <h1>Register</h1>
-                <hr />
-                <div>
-                    <label htmlFor="username">Username</label>
-                    <input
-                        type="text"
-                        id="username"
-                        onChange={(e) => setUsername(e.target.value)}
-                        placeholder="Username"
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        onChange={(e) => setPassword(e.target.value)}
-                        placeholder="Password"
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="confirm-password">Confirm Password</label>
-                    <input
-                        type="password"
-                        id="confirm-password"
-                        onChange={(e) => setPassword2(e.target.value)}
-                        placeholder="Confirm Password"
-                        required
-                    />
-                    <p>
-                        {password2 !== password ? 'Passwords do not match' : ''}
-                    </p>
-                </div>
-                <button type="submit">Register</button>
-            </form>
-        </section>
+      <Flex
+        minH={'100vh'}
+        align={'center'}
+        justify={'center'}
+        bg={useColorModeValue('gray.50', 'gray.800')}>
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+          <Stack align={'center'}>
+            <Heading fontSize={'4xl'} textAlign={'center'}>
+              Sign up
+            </Heading>
+            <Text fontSize={'lg'} color={'gray.600'}>
+              to enjoy all of our cool features ✌️
+            </Text>
+          </Stack>
+          <Box
+            rounded={'lg'}
+            bg={useColorModeValue('white', 'gray.700')}
+            boxShadow={'lg'}
+            p={8}>
+            <Stack spacing={4}>
+              
+              <FormControl id="name" isRequired>
+                <FormLabel>User name</FormLabel>
+                <Input type="name" />
+              </FormControl>
+              <FormControl id="password" isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                  <Input type={showPassword ? 'text' : 'password'} />
+                  <InputRightElement h={'full'}>
+                    <Button
+                      variant={'ghost'}
+                      onClick={() =>
+                        setShowPassword((showPassword) => !showPassword)
+                      }>
+                      {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </FormControl>
+              <Stack spacing={10} pt={2}>
+                <Button
+                  onClick={handleSubmit}
+                  loadingText="Submitting"
+                  size="lg"
+                  bg={'blue.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'blue.500',
+                  }}>
+                  Sign up
+                </Button>
+              </Stack>
+              <Stack pt={6}>
+                <Text align={'center'}>
+                  Already have an account? <Link href="/login" color={'blue.400'}>Login</Link>
+                </Text>
+              </Stack>
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
     );
-}
-
-export default Register;
+  }
